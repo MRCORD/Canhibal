f3512b22c405e7bb5ca694c6cb84faf51d607446
MRCORD <orivas@actualize.ca>
2019-06-05 16:05:08 -0500

    update

--- Scrap.py
+++ Scrap.py
@@ -1,20 +1,17 @@
 import requests
-
-import pandas as pd
 from bs4 import BeautifulSoup
 
-url = 'https://www.fundserv.com/tools-and-training/tools/fund-profiles/'
-response = requests.get(url)
 
-bs_html = BeautifulSoup(response, 'lxml')
-headers = [header.text for header in bs_html.find_all('th')]
-headers_string = ""
-for head in headers:
-    headers_string = headers_string + head + ","
-headers_string = headers_string[:-1] + "\n"
+url = "https://www.fundserv.com/tools-and-training/tools/fund-profiles/"
+page = requests.get(url)
 
-rows = []
-table = bs_html.find("table")
-for row in table.find_all('td'):
-    rows.append(row.getText())
-    
+print(page.content)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
+
+tb = soup.find('table', class_='data-t wpDataTable')
+
+for link in tb.find_all('tr'):
+    name = link.find('td')
+    print(name.get_text('style'))
